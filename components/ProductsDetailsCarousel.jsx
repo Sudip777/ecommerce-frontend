e1b5a7a0edd879f1a9e6969884859961b7e11009
Sudip Parajuli <sudipparajuli06@gmail.com>
2023-06-17 14:43:44 +0545

    Initial commit

---
+++ components/ProductsDetailsCarousel.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+const ProductsDetailsCarousel =
+  ({images}) => {
+    return (
+      <div className="text-white text-[20px] w-full max-w-[1360px] mx-auto sticky top-[50px]">
+        <Carousel
+          infiniteLoop={true}
+          showIndicators={
+            false
+          }
+          showStatus={false}
+          thumbWidth={60}
+          className="productCarousel">
+
+            {images?.map((img) =>{
+              return(
+              <img key ={img.id}
+              src={img.attributes.url}
+              alt={img.attributes.name}
+            />
+              );
+            })}
+        </Carousel>
+      </div>
+    );
+  };
+
+export default ProductsDetailsCarousel;
